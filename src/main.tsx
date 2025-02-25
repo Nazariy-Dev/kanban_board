7c9521015289d19867633542734ac04a955be37b
Nazariy-Dev <139453365+Nazariy-Dev@users.noreply.github.com>
2025-02-25 12:08:00 +0200

    rect-router config was changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import {Provider} from "./components/ui/provider"
 import ReactDOM from "react-dom/client"
 import App from "./App/App.tsx"
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    RouterProvider,
-    Route,
-} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom'
 
-
- const router = createBrowserRouter(
-    createRoutesFromElements(<Route path='/' element={<App/>}></Route>)
-);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider>
-        <RouterProvider router={router}/>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </Provider>
 )
